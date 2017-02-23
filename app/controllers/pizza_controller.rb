2b243736d2a1742140adaad1c6206b10354b265e
Chad Marshall <cmarshall@trendshift.com>
2017-02-23 15:31:53 -0700

    committing to push to origin

--- app/controllers/pizza_controller.rb
+++ app/controllers/pizza_controller.rb
@@ -5,7 +5,9 @@ class PizzaController < ApplicationController
   end
 
   def edit
-    render html: "<strong>still working on </strong><a href='#' onclick='history.go(-1); return false;'>Back</a>".html_safe
+    # render html: "<strong>still working on </strong><a href='#' onclick='history.go(-1); return false;'>Back</a>".html_safe
+    output = "<p>im still working on this, <a href='" + pizza_url  + "'>click to go back</a></p>"
+    render html: output.html_safe
   end
 
   def update
